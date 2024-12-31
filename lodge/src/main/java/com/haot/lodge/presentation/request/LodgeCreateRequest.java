8a7c9b1574b95c3f5166c128fc18fc7d52638e1b
siwon <muncool39@gmail.com>
2025-01-01 04:28:16 +0900

    [Init] 숙소 생성 요청 객체 필드 추가

--- lodge/src/main/java/com/haot/lodge/presentation/request/LodgeCreateRequest.java
+++ lodge/src/main/java/com/haot/lodge/presentation/request/LodgeCreateRequest.java
@@ -1,5 +1,25 @@
 package com.haot.lodge.presentation.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
 public record LodgeCreateRequest(
+        @NotBlank String name,
+        @NotBlank String description,
+        @NotBlank String address,
+        @NotNull Integer term,
+        @NotNull Double basicPrice,
+        @NotNull List<MultipartFile> images,
+        @NotNull List<String> imageTitles,
+        @NotNull List<String> imageDescriptions,
+        @NotNull Integer maxReservationDay,
+        @NotNull Integer maxPersonnel,
+        String customization,
+        @NotNull LocalDate startDate,
+        @NotNull LocalDate endDate,
+        List<LocalDate> excludeDates
 ) {
 }
