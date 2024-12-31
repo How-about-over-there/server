621df3e7c5e52f20591f3c1a570c0f6a8192810e
wooseok <wooseok6008@gmail.com>
2024-12-31 12:21:34 +0900

    [feat] Reservation: 예약 엔티티 생성 #4
    - Reservation, ReservationStatus 예약 상태값

---
+++ reservation/src/main/java/com/haot/reservation/domain/model/Reservation.java
@@ -0,0 +1,55 @@
+package com.haot.reservation.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "p_reservation")
+public class Reservation {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "reservation_id", updatable = false, nullable = false)
+  private String reservationId;
+
+  @Column(name = "user_id", nullable = false)
+  private String userId;
+
+  @Column(name = "lodge_name", nullable = false)
+  private String lodgeName;
+
+  @Column(name = "check_in_date", nullable = false)
+  private LocalDate checkInDate;
+
+  @Column(name = "check_out_date", nullable = false)
+  private LocalDate checkOutDate;
+
+  @Column(name = "num_guests", nullable = false)
+  private Integer numGuests;
+
+  @Column(name = "request", nullable = false)
+  private String request;
+
+  @Column(name = "total_price", nullable = false)
+  private Integer totalPrice;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "reservation_status", nullable = false)
+  private ReservationStatus status;
+}
---
+++ reservation/src/main/java/com/haot/reservation/domain/model/ReservationStatus.java
@@ -0,0 +1,15 @@
+package com.haot.reservation.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ReservationStatus {
+
+  PENDING("예약 대기"),
+  CANCELED("예약 취소"),
+  COMPLETED("예약 완료");
+
+  private final String description;
+}
