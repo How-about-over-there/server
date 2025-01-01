8f817cc0015ba501b0232f250733a7f2f3a3ab8d
siwon <muncool39@gmail.com>
2025-01-01 18:07:53 +0900

    [Fix] 응답시의 상태코드 서비스에 맞게 변경

--- lodge/src/main/java/com/haot/lodge/common/response/ApiResponse.java
+++ lodge/src/main/java/com/haot/lodge/common/response/ApiResponse.java
@@ -6,14 +6,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T> (
         String statusCode,
+        String status,
         String message,
         T data
 ) {
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>( "2000","API 요청에 성공했습니다", data);
+        return new ApiResponse<>( "5000", "SUCCESS","API 요청에 성공했습니다", data);
     }
 
     public static ApiResponse<Void> success() {
-        return new ApiResponse<>("2000","API 요청에 성공했습니다", null);
+        return new ApiResponse<>("5000", "SUCCESS","API 요청에 성공했습니다", null);
     }
 }
