2d0bb23b3a02f793a5bd92dbedc99e04d84229eb
wooseok <wooseok6008@gmail.com>
2025-01-02 11:59:45 +0900

    [Feat] Reservation: 예약 상태값 추가 #24
    - PENDING_VIRTUAL_ACCOUNT(가상계좌 대기 상태) 상태값 추가

--- reservation/src/main/java/com/haot/reservation/domain/model/ReservationStatus.java
+++ reservation/src/main/java/com/haot/reservation/domain/model/ReservationStatus.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ReservationStatus {
 
+  PENDING_VIRTUAL_ACCOUNT("가상계좌 결제 대기"),
   PENDING("결제 대기"),
   CANCELED("예약 취소"),
   COMPLETED("예약 완료");
