deb184c86fe264d5dbad398f40e8dbb66dbecac8
kimsung3113 <kimsung1017@naver.com>
2025-01-02 21:56:31 +0900

    [feat] coupons : ApiResponse 응답들 생성, ErrorCode 생성, customException 생성 #15

---
+++ coupon/src/main/java/com/haot/coupon/common/exceptions/CustomCouponException.java
@@ -0,0 +1,11 @@
+package com.haot.coupon.common.exceptions;
+
+import com.haot.coupon.common.response.ResCodeIfs;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomCouponException extends RuntimeException{
+    ResCodeIfs resCode;
+}
---
+++ coupon/src/main/java/com/haot/coupon/common/response/enums/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.haot.coupon.common.response.enums;
+
+import com.haot.coupon.common.response.ResCodeIfs;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode implements ResCodeIfs {
+
+    VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "0009", "Validation failed"),
+
+    // 쿠폰 exception
+    DISCOUNT_RATE_EXCEPTION(HttpStatus.BAD_REQUEST, "4002", "wrong discountRate");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+}
