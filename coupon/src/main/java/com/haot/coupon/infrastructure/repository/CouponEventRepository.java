fdf71d3d2ad32e60791245dab45eca49fc102adb
kimsung3113 <kimsung1017@naver.com>
2025-01-02 22:35:30 +0900

    [feat] coupons : ReservationCoupon entity, repository 생성 #15

--- coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponEventRepository.java
+++ coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponEventRepository.java
@@ -1,6 +1,5 @@
 package com.haot.coupon.infrastructure.repository;
 
-import com.haot.coupon.domain.model.Coupon;
 import com.haot.coupon.domain.model.CouponEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
