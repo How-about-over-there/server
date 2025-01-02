c022d6f73d538ba9cb51c901bdb0b68ac8d40280
kimsung3113 <kimsung1017@naver.com>
2025-01-02 22:28:36 +0900

    [rename] coupons : coupon, usercoupon repository 위치 변경 #15

--- coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponRepository.java
+++ coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponRepository.java
@@ -1,4 +1,4 @@
-package com.haot.coupon.infrastructure;
+package com.haot.coupon.infrastructure.repository;
 
 import com.haot.coupon.domain.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
--- coupon/src/main/java/com/haot/coupon/infrastructure/repository/UserCouponRepository.java
+++ coupon/src/main/java/com/haot/coupon/infrastructure/repository/UserCouponRepository.java
@@ -1,4 +1,4 @@
-package com.haot.coupon.infrastructure;
+package com.haot.coupon.infrastructure.repository;
 
 import com.haot.coupon.domain.model.UserCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
