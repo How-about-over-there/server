ea66edeaa9a170c44e5b172f91852f90c53099b9
jelliclesu <jelliclesu@naver.com>
2025-01-03 16:08:51 +0900

    [Feat] Point: 포인트/포인트 내역 Entity 생성 #34

---
+++ point/src/main/java/com/haot/point/domain/model/PointHistory.java
@@ -0,0 +1,47 @@
+package com.haot.point.domain.model;
+
+import com.haot.point.domain.enums.PointStatus;
+import com.haot.point.domain.enums.PointType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "p_point_history", schema = "point")
+public class PointHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "point_history_id")
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "point_id", nullable = false)
+    private Point point;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "points", nullable = false)
+    @Min(0)
+    private Double points;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private PointType type;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "expired_at", nullable = false)
+    private LocalDateTime expiredAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private PointStatus status;
+}
