b95aee2290d12e61d4fe2f71c9add6db598cf002
siwon <muncool39@gmail.com>
2025-01-03 15:57:13 +0900

    [Feat] 에외 처리

--- lodge/src/main/java/com/haot/lodge/common/config/ValidationMessageConfig.java
+++ lodge/src/main/java/com/haot/lodge/common/config/ValidationMessageConfig.java
@@ -1,4 +1,18 @@
 package com.haot.lodge.common.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+
+@Configuration
 public class ValidationMessageConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:validation-messages-ko");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
--- lodge/src/main/java/com/haot/lodge/common/exception/LodgeException.java
+++ lodge/src/main/java/com/haot/lodge/common/exception/LodgeException.java
@@ -1,7 +1,12 @@
 package com.haot.lodge.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class LodgeException extends RuntimeException {
-  public LodgeException(String message) {
-    super(message);
-  }
+    private final ErrorCode errorCode;
+
+    public LodgeException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
 }
