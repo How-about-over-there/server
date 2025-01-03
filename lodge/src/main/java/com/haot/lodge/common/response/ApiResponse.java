3eafe4dbaad9ce6c030ee280a56a936d0fd2a0f5
siwon <muncool39@gmail.com>
2025-01-03 15:46:29 +0900

    [Feat] 실패시의 응답 추가

--- lodge/src/main/java/com/haot/lodge/common/response/ApiResponse.java
+++ lodge/src/main/java/com/haot/lodge/common/response/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.haot.lodge.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.haot.lodge.common.exception.ErrorCode;
 
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -16,5 +17,13 @@ public record ApiResponse<T> (
 
     public static ApiResponse<Void> success() {
         return new ApiResponse<>("5000", "SUCCESS","API 요청에 성공했습니다", null);
+    }
+
+    public static ApiResponse<Void> fail(ErrorCode errorCode) {
+        return new ApiResponse<>(errorCode.getCode(), "FAIL", errorCode.getMessage(), null);
+    }
+
+    public static <T> ApiResponse<T> fail(ErrorCode errorCode, T data) {
+        return new ApiResponse<>(errorCode.getCode(), "ERROR", errorCode.getMessage(), data);
     }
 }
