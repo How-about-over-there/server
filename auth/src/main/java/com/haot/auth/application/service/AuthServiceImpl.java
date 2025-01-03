4fa646e6795b62a68eb838ff3f4d10b64e76cd40
chanwookK <kan0202@naver.com>
2025-01-03 11:52:10 +0900

    [Feat] Auth: 회원가입 API Service 단 구현 #22

---
+++ auth/src/main/java/com/haot/auth/application/service/AuthServiceImpl.java
@@ -0,0 +1,32 @@
+package com.haot.auth.application.service;
+
+import com.haot.auth.application.dto.req.AuthLoginRequest;
+import com.haot.auth.application.dto.req.AuthSignupRequest;
+import com.haot.auth.application.dto.res.AuthLoginResponse;
+import com.haot.auth.application.dto.res.AuthSignupResponse;
+import com.haot.auth.application.feign.adapter.UserClientAdapter;
+import com.haot.auth.common.util.JwtUtil;
+import com.haot.auth.domain.enums.Role;
+import com.haot.auth.infrastructure.feign.dto.FeignUserLoginValidationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+  private final UserClientAdapter userClientAdapter;
+
+  @Override
+  public AuthSignupResponse signup(AuthSignupRequest request) {
+    // business logic : Role 의 디폴트 값을 USER 로 셋팅
+    Role userRoleToSave = (request.role() == null ? Role.USER : request.role());
+
+    // return : user-service 에 유저 저장을 요청한 결과를 반환
+    // email 이 중복 될 경우 user-service 에서 예외 처리됨.
+    return AuthSignupResponse.builder()
+        .userId(userClientAdapter.createUser(request, userRoleToSave))
+        .build();
+  }
+
+}
