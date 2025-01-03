211080885fd703b4c7b1f419fb809f63b6d27289
kimsung3113 <kimsung1017@naver.com>
2025-01-03 23:02:09 +0900

    [feat] coupons : 모든 ENUM stream 체크 후 맞지 않으면 Error 발생 메서드 추가 #38

--- coupon/src/main/java/com/haot/coupon/domain/model/enums/ReservationCouponStatus.java
+++ coupon/src/main/java/com/haot/coupon/domain/model/enums/ReservationCouponStatus.java
@@ -1,7 +1,11 @@
 package com.haot.coupon.domain.model.enums;
 
+import com.haot.coupon.common.exceptions.CustomCouponException;
+import com.haot.coupon.common.response.enums.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.stream.Stream;
 
 @Getter
 @AllArgsConstructor
@@ -13,4 +17,13 @@ public enum ReservationCouponStatus {
     EXPIRED("쿠폰 만료");
 
     private final String description;
+
+    // String 값으로 해당하는 Enum 값을 반환하는 메서드
+    public static CouponStatus checkReservationCouponStatus(String status) {
+        return Stream.of(CouponStatus.values())
+                .filter(couponStatus -> couponStatus.name().equalsIgnoreCase(status))
+                .findFirst()
+                .orElseThrow(() -> new CustomCouponException(ErrorCode.RESERVATION_STATUS_NOT_MATCH));
+    }
+
 }
