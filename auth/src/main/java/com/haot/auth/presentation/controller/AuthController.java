480ae090a0ec86e8b7d32d60fd4f1c068859d179
chanwookK <kan0202@naver.com>
2025-01-03 11:43:55 +0900

    [Feat] Auth: 회원가입 API Controller 단 구현 #22

---
+++ auth/src/main/java/com/haot/auth/presentation/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.haot.auth.presentation.controller;
+
+import com.haot.auth.application.dto.req.AuthLoginRequest;
+import com.haot.auth.application.dto.req.AuthSignupRequest;
+import com.haot.auth.application.dto.res.AuthLoginResponse;
+import com.haot.auth.application.dto.res.AuthSignupResponse;
+import com.haot.auth.application.service.AuthService;
+import com.haot.auth.common.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("/signup")
+  public ApiResponse<AuthSignupResponse> signup(@Valid @RequestBody AuthSignupRequest request){
+    return ApiResponse.success(authService.signup(request));
+  }
+}
