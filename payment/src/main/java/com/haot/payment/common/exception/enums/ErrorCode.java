ca2091cced7d7cdc694173abb0ad232b5b5885cb
jelliclesu <jelliclesu@naver.com>
2025-01-04 23:32:15 +0900

    [Feat] Payment: ErrorCode 추가 및 error 응답 추가 #43

---
+++ payment/src/main/java/com/haot/payment/common/exception/enums/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.haot.payment.common.exception.enums;
+
+import com.haot.payment.common.response.ResCodeIfs;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode implements ResCodeIfs {
+
+    // 0000: Common Error
+    INTERNAL_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "0000", "예기치 못한 오류가 발생했습니다."),
+    NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "0001", "존재하지 않는 리소스입니다."),
+    INVALID_VALUE_EXCEPTION(HttpStatus.BAD_REQUEST, "0002", "올바르지 않은 요청 값입니다."),
+    UNAUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED, "0003", "권한이 없는 요청입니다."),
+    ALREADY_DELETE_EXCEPTION(HttpStatus.BAD_REQUEST, "0004", "이미 삭제된 리소스입니다."),
+    FORBIDDEN_EXCEPTION(HttpStatus.FORBIDDEN, "0005", "인가되지 않는 요청입니다."),
+    ALREADY_EXIST_EXCEPTION(HttpStatus.BAD_REQUEST, "0006", "이미 존재하는 리소스입니다."),
+    INVALID_SORT_EXCEPTION(HttpStatus.BAD_REQUEST, "0007", "올바르지 않은 정렬 값입니다."),
+    SEARCH_LOG_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "0008", "존재하지 않는 검색 로그입니다."),
+    VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "0009", "입력 데이터 유효성 검증에 실패했습니다."),
+
+    // 8000: Payment Error
+    PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "80001", "결제 정보를 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
--- payment/src/main/java/com/haot/payment/common/response/ApiResponse.java
+++ payment/src/main/java/com/haot/payment/common/response/ApiResponse.java
@@ -2,11 +2,14 @@ package com.haot.payment.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.List;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T> (
         String statusCode,
         String status,
         String message,
+        List<String> errorList,
         T data
 ) {
     public static <T> ApiResponse<T> success(T data) {
@@ -15,5 +18,21 @@ public record ApiResponse<T> (
 
     public static ApiResponse<Void> success() {
         return new ApiResponse<>("8000", "Success","API 요청에 성공했습니다", null);
+    }
+
+    public static ApiResponse<Object> error(ResCodeIfs resCodeIfs){
+        return new ApiResponse<>(resCodeIfs.getCode(), "Error", resCodeIfs.getMessage(), null, null);
+    }
+
+    public static ApiResponse<Object> error(ResCodeIfs resCodeIfs, List<String> errorList) {
+        return new ApiResponse<>(resCodeIfs.getCode(), "Error", resCodeIfs.getMessage(), errorList, null);
+    }
+
+    public ApiResponse(String statusCode, String status, String message, T data){
+        this(statusCode, status, message, null, data);
+    }
+
+    public ApiResponse(String statusCode, String status, String message, List<String> errorList){
+        this(statusCode, status, message, errorList, null);
     }
 }
