ce73772e2dfa4b4c259ece527276d14b1de5609a
jelliclesu <jelliclesu@naver.com>
2025-01-04 20:37:42 +0900

    [Fix] Payment: 결제 상태 Enum 값 추가 #43

--- payment/src/main/java/com/haot/payment/domain/enums/PaymentStatus.java
+++ payment/src/main/java/com/haot/payment/domain/enums/PaymentStatus.java
@@ -6,10 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PaymentStatus {
-    READY("결제 대기"),
+    READY("결제 준비"),
+    VIRTUAL_ACCOUNT_ISSUED("가상계좌 발급 완료"),
+    PAY_PENDING("결제 완료 대기"),
     PAID("결제 완료"),
     FAILED("결제 실패"),
-    CANCELED("결제 취소");
+    CANCELLED("결제 취소"),
+    PARTIAL_CANCELLED("결제 부분 취소");
 
     private final String description;
 }
