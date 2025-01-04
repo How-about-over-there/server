1f3704d4f649f79e8b9b6cac981d2fa41df6df6e
kimsung3113 <kimsung1017@naver.com>
2025-01-04 17:08:03 +0900

    [feat] coupons :  SuccessCode 생성 #38

---
+++ coupon/src/main/java/com/haot/coupon/common/response/enums/SuccessCode.java
@@ -0,0 +1,19 @@
+package com.haot.coupon.common.response.enums;
+
+import com.haot.coupon.common.response.ResCodeIfs;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessCode implements ResCodeIfs {
+
+    CREATE_COUPON_SUCCESS(HttpStatus.CREATED, "4000", "쿠폰이 성공적으로 생성되었습니다."),
+    CREATE_EVENT_SUCCESS(HttpStatus.CREATED, "4000", "이벤트가 성공적으로 생성되었습니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
