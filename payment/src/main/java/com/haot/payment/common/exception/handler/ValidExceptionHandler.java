81e489cf8826925df0374f852cdb0253d688047d
jelliclesu <jelliclesu@naver.com>
2025-01-04 23:37:51 +0900

    [Feat] Payment: ValidExceptionHandler 추가 #43

---
+++ payment/src/main/java/com/haot/payment/common/exception/handler/ValidExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.haot.payment.common.exception.handler;
+
+import com.haot.payment.common.exception.enums.ErrorCode;
+import com.haot.payment.common.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j(topic = "ValidExceptionHandler")
+@Order(value = Integer.MAX_VALUE - 1)
+@RestControllerAdvice
+public class ValidExceptionHandler {
+
+    // MethodArgumentNotValidException 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+
+        List<String> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " : " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ErrorCode.VALIDATION_EXCEPTION, errors));
+    }
+
+}
