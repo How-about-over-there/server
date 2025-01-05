77c6da0fe5272e34b7a8d132252b0990a4176b80
siwon <muncool39@gmail.com>
2025-01-05 17:40:36 +0900

    [Feat] 응답 객체에 엔티티에서 변환하는 메서드 추가

--- lodge/src/main/java/com/haot/lodge/application/response/lodgeImageResponse.java
+++ lodge/src/main/java/com/haot/lodge/application/response/lodgeImageResponse.java
@@ -1,6 +1,7 @@
 package com.haot.lodge.application.response;
 
 
+import com.haot.lodge.domain.model.LodgeImage;
 import lombok.Builder;
 
 @Builder
@@ -10,4 +11,12 @@ public record LodgeImageResponse(
         String description,
         String url
 ){
+    public static LodgeImageResponse from(LodgeImage lodgeImage) {
+        return new LodgeImageResponse(
+                lodgeImage.getId(),
+                lodgeImage.getTitle(),
+                lodgeImage.getDescription(),
+                lodgeImage.getUrl()
+        );
+    }
 }
