e5ef6ff2cdc5db8841a5b2b73f1a38f0fd607d5a
siwon <muncool39@gmail.com>
2025-01-06 21:54:50 +0900

    [Feat] 커스텀 쿼리

---
+++ lodge/src/main/java/com/haot/lodge/domain/repository/LodgeDateCustomRepository.java
@@ -0,0 +1,13 @@
+package com.haot.lodge.domain.repository;
+
+import com.haot.lodge.domain.model.Lodge;
+import com.haot.lodge.domain.model.LodgeDate;
+import java.time.LocalDate;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+public interface LodgeDateCustomRepository {
+    Slice<LodgeDate> findAllLodgeDateByRange(
+            Pageable pageable,Lodge lodge, LocalDate start, LocalDate end
+    );
+}
