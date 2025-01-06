3b50a782cfa3740a846542dfad20a6203d25cc8b
kangchanwook <kan0202@naver.com>
2025-01-06 14:57:08 +0900

    [Feat] Auth: login API 관련 Service 단 구현 #22

--- auth/src/main/java/com/haot/auth/application/service/AuthService.java
+++ auth/src/main/java/com/haot/auth/application/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.haot.auth.application.service;
 
+import com.haot.auth.application.dto.req.AuthLoginRequest;
 import com.haot.auth.application.dto.req.AuthSignupRequest;
+import com.haot.auth.application.dto.res.AuthLoginResponse;
 import com.haot.auth.application.dto.res.AuthSignupResponse;
 
 public interface AuthService {
 
   AuthSignupResponse signup(AuthSignupRequest request);
+
+  AuthLoginResponse login(AuthLoginRequest request);
 }
