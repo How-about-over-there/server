78116c9130a0ba3196f0d3863fd60a5c022a0adb
jelliclesu <jelliclesu@naver.com>
2025-01-07 15:25:30 +0900

    [Feat] Point: ValidExceptionHandler 추가 #61

---
+++ point/src/main/java/com/haot/point/infrastructure/config/ValidationMessageConfig.java
@@ -0,0 +1,18 @@
+package com.haot.point.infrastructure.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class ValidationMessageConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:validation-messages-ko"); // custom 파일명 사용
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
