5cdb256c2ad4f5d8a4621afaeea01c68dac2d81f
chanwookK <kan0202@naver.com>
2025-01-07 11:42:15 +0900

    [Refactor] JwtUtils static import로 변경

--- auth/src/main/java/com/haot/auth/common/util/JwtUtils.java
+++ auth/src/main/java/com/haot/auth/common/util/JwtUtils.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Base64;
@@ -72,7 +73,7 @@ public class JwtUtils {
     try {
       Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
       return true;
-    } catch (SecurityException | MalformedJwtException | io.jsonwebtoken.security.SignatureException e) {
+    } catch (SecurityException | MalformedJwtException | SignatureException e) {
       log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
     } catch (ExpiredJwtException e) {
       log.error("Expired JWT token, 만료된 JWT token 입니다.");
