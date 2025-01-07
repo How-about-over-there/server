ae4697658b838cca964afb6cec5f591f49715d59
kimsung3113 <kimsung1017@naver.com>
2025-01-07 23:43:56 +0900

    [fix] coupons : errorCode, SuccessCode 수정, EventStatus 체크 코드 추가 #56

--- coupon/src/main/java/com/haot/coupon/common/response/enums/SuccessCode.java
+++ coupon/src/main/java/com/haot/coupon/common/response/enums/SuccessCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode implements ResCodeIfs {
 
     CREATE_COUPON_SUCCESS(HttpStatus.CREATED, "4000", "쿠폰이 성공적으로 생성되었습니다."),
-    CREATE_EVENT_SUCCESS(HttpStatus.CREATED, "4000", "이벤트가 성공적으로 생성되었습니다.")
+    CREATE_EVENT_SUCCESS(HttpStatus.CREATED, "4000", "이벤트가 성공적으로 생성되었습니다."),
+    CUSTOMER_ISSUED_COUPON_SUCCESS(HttpStatus.OK, "4000", "사용자가 쿠폰을 성공적으로 발급하였습니다.");
     ;
 
     private final HttpStatus httpStatus;
