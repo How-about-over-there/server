3c82c5f76b4b78aef611678d9a3547332e2c5725
jelliclesu <jelliclesu@naver.com>
2025-01-07 15:19:32 +0900

    [Feat] Point: ErrorCode 추가 및 error 응답 추가 #61

--- point/src/main/java/com/haot/point/common/response/ApiResponse.java
+++ point/src/main/java/com/haot/point/common/response/ApiResponse.java
@@ -2,18 +2,37 @@ package com.haot.point.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.List;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T> (
         String statusCode,
         String status,
         String message,
+        List<String> errorList,
         T data
 ) {
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>("9000", "Success", "API 요청에 성공했습니다", data);
+        return new ApiResponse<>("9000", "SUCCESS", "API 요청에 성공했습니다", data);
     }
 
     public static ApiResponse<Void> success() {
-        return new ApiResponse<>("9000", "Success","API 요청에 성공했습니다", null);
+        return new ApiResponse<>("9000", "SUCCESS","API 요청에 성공했습니다", null);
+    }
+
+    public static ApiResponse<Object> error(ResCodeIfs resCodeIfs){
+        return new ApiResponse<>(resCodeIfs.getCode(), "ERROR", resCodeIfs.getMessage(), null, null);
+    }
+
+    public static ApiResponse<Object> error(ResCodeIfs resCodeIfs, List<String> errorList) {
+        return new ApiResponse<>(resCodeIfs.getCode(), "ERROR", resCodeIfs.getMessage(), errorList, null);
+    }
+
+    public ApiResponse(String statusCode, String status, String message, T data){
+        this(statusCode, status, message, null, data);
+    }
+
+    public ApiResponse(String statusCode, String status, String message, List<String> errorList) {
+        this(statusCode, status, message, errorList, null);
     }
 }
