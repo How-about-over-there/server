c10b511d177aad54a248dc704d5b6de14953c24e
jelliclesu <jelliclesu@naver.com>
2025-01-07 16:03:03 +0900

    [Feat] Point: of 메서드 추가 #61

--- point/src/main/java/com/haot/point/application/dto/response/PointResponse.java
+++ point/src/main/java/com/haot/point/application/dto/response/PointResponse.java
@@ -1,7 +1,17 @@
 package com.haot.point.application.dto.response;
 
+import com.haot.point.domain.model.Point;
+
 public record PointResponse(
         String pointId,
         String userId,
         Double totalPoints
-) {}
+) {
+    public static PointResponse of(Point point) {
+        return new PointResponse(
+                point.getId(),
+                point.getUserId(),
+                point.getTotalPoints()
+        );
+    }
+}
