0fab2742d7c0a80c6b46a8a0f87efca9d1480e0e
siwon <muncool39@gmail.com>
2025-01-08 16:21:59 +0900

    [Feat] 숙소 조회 및 검색 쿼리

--- lodge/src/main/java/com/haot/lodge/domain/repository/LodgeCustomRepository.java
+++ lodge/src/main/java/com/haot/lodge/domain/repository/LodgeCustomRepository.java
@@ -1,4 +1,19 @@
 package com.haot.lodge.domain.repository;
 
+import com.haot.lodge.domain.model.Lodge;
+import java.time.LocalDate;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+
 public interface LodgeCustomRepository {
+    Slice<Lodge> findAllByConditionOf(
+            Pageable pageable,
+            String name,
+            String address,
+            Integer maxReservationDay,
+            Integer maxPersonnel,
+            LocalDate checkInDate,
+            LocalDate checkOutDate
+    );
 }
