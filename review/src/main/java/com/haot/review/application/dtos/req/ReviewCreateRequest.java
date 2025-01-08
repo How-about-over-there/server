baf196c6afaf57ae89d2b4ff841406a62f7a2d49
wooseok <wooseok6008@gmail.com>
2025-01-08 18:19:29 +0900

    [Fix] Review: @NotBlank, 인코딩 적용 #69

--- review/src/main/java/com/haot/review/application/dtos/req/ReviewCreateRequest.java
+++ review/src/main/java/com/haot/review/application/dtos/req/ReviewCreateRequest.java
@@ -1,13 +1,12 @@
 package com.haot.review.application.dtos.req;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public record ReviewCreateRequest(
-    @NotNull String contents,
-    @NotNull String lodgeId,
+    @NotBlank String contents,
+    @NotBlank String lodgeId,
     List<MultipartFile> images
 ) {
 
