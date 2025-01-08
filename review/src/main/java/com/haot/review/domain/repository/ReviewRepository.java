bd21428b27399d12aad7b4b7edce69f0a22db749
wooseok <wooseok6008@gmail.com>
2025-01-08 18:08:01 +0900

    [Fix] Review: ReviewRepository 타입 변경 #69

--- review/src/main/java/com/haot/review/domain/repository/ReviewRepository.java
+++ review/src/main/java/com/haot/review/domain/repository/ReviewRepository.java
@@ -3,6 +3,6 @@ package com.haot.review.domain.repository;
 import com.haot.review.domain.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, String> {
 
 }
