5be30889892e6b8cd5b42f24c4208155096baa8d
jelliclesu <jelliclesu@naver.com>
2025-01-08 14:11:12 +0900

    [Feat] Payment: 본인 결제 단건 조회 API 구현 (#71)
    * [Fix] Payment: 변경된 ErrorCode 반영 #68
    
    * [Feat] Payment: 본인 결제 단건 조회 구현 #68

--- payment/src/main/java/com/haot/payment/infrastructure/repository/PaymentRepository.java
+++ payment/src/main/java/com/haot/payment/infrastructure/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.haot.payment.infrastructure.repository;
 import com.haot.payment.domain.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, String> {
+    Optional<Payment> findByIdAndIsDeletedFalse(String paymentId);
 }
