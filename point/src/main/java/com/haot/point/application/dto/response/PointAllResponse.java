db7170a4f9257ee25576db6f501207d71edbc7d6
jelliclesu <jelliclesu@naver.com>
2025-01-08 18:45:31 +0900

    [Feat] Point: of 메서드 추가 #73

--- point/src/main/java/com/haot/point/application/dto/response/PointAllResponse.java
+++ point/src/main/java/com/haot/point/application/dto/response/PointAllResponse.java
@@ -1,5 +1,8 @@
 package com.haot.point.application.dto.response;
 
+import com.haot.point.domain.model.Point;
+import com.haot.point.domain.model.PointHistory;
+
 import java.time.LocalDateTime;
 
 public record PointAllResponse(
@@ -12,4 +15,18 @@ public record PointAllResponse(
         String description,
         LocalDateTime expiredAt,
         String status
-) {}
+) {
+    public static PointAllResponse of(Point point, PointHistory pointHistory) {
+        return new PointAllResponse(
+                point.getId(),
+                pointHistory.getId(),
+                point.getUserId(),
+                pointHistory.getPoints(),
+                point.getTotalPoints(),
+                pointHistory.getType().name(),
+                pointHistory.getDescription(),
+                pointHistory.getExpiredAt(),
+                pointHistory.getStatus().name()
+        );
+    }
+}
