b89d4b01a2562c6afdac5c3a7ef9967879b73335
kimsung3113 <kimsung1017@naver.com>
2025-01-08 14:01:59 +0900

    [feat] coupons : 이벤트 상세 조회 API 생성 #56

--- coupon/src/main/java/com/haot/coupon/presentation/controller/EventController.java
+++ coupon/src/main/java/com/haot/coupon/presentation/controller/EventController.java
@@ -2,7 +2,9 @@ package com.haot.coupon.presentation.controller;
 
 import com.haot.coupon.application.dto.request.events.EventSearchRequest;
 import com.haot.coupon.application.dto.response.events.EventSearchResponse;
+import com.haot.coupon.application.service.EventService;
 import com.haot.coupon.common.response.ApiResponse;
+import com.haot.coupon.common.response.enums.SuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -18,20 +20,13 @@ import java.util.List;
 @RequestMapping("/api/v1/events")
 public class EventController {
 
+    private final EventService eventService;
+
+    // TODO 전체 조회할때 일반 user는 삭제된 event는 보여주지 않게!
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{eventId}")
-    public ApiResponse<EventSearchResponse> getEvent(@PathVariable String eventId) {
-
-        EventSearchResponse response = EventSearchResponse.builder()
-                .eventId(eventId)
-                .couponId("skfjalsdkfsalkdnf")
-                .eventStartDate(LocalDateTime.now())
-                .eventEndDate(LocalDateTime.now().plusWeeks(1))
-                .eventName("테스트 이벤트")
-                .eventDescription("연초 숙박 이벤트 ~")
-                .build();
-
-        return ApiResponse.success(response);
+    public ApiResponse<EventSearchResponse> getEvent(@PathVariable(value = "eventId") String eventId) {
+        return ApiResponse.SUCCESS(SuccessCode.GET_DETAIL_EVENT_SUCCESS, eventService.getEvent(eventId));
     }
 
     @ResponseStatus(HttpStatus.OK)
