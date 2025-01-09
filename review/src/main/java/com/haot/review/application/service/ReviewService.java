356484f694c38250c75fe1893660754861f911e4
wooseok50 <155416976+wooseok50@users.noreply.github.com>
2025-01-09 11:08:12 +0900

    [Feat] Review: 리뷰 생성 API (#84)
    * [Feat] Review: Gradle 추가 및 yml 작성 #69
    
    - Review 초기 서버 설정
    
    * [Feat] Review: Review, ReviewImage 엔티티 설계 #69
    
    - create 메서드 생성
    
    * [Fix] Review: yml application.name 수정 #69
    
    * [Feat] Review: 리뷰 생성 API 구현 #69
    
    - 리뷰 생성시 Review, ReviewImage 생성
    - s3 연동이 아직으로 mockS3Service 구현
    
    * [Feat] Review: Review 패키지에 서브 모듈 추가 #69
    
    - BaseEntity 적용
    - RoleCheck 적용
    
    * [Feat] Review: validation-messages-ko.properties #69
    
    * [Fix] Review: RoleCheck 제거 gateway 이후에 개발 #69
    
    * [Fix] Review: 테이블에 lodgeId 추가 #69
    
    * [Fix] Review: ReviewRepository 타입 변경 #69
    
    * [Fix] Review: @NotBlank, 인코딩 적용 #69
    
    * [Fix] Review: validation-messages-ko.properties 인코딩 수정 #69
    
    ---------
    
    Co-authored-by: wooseok <wooseok6008@gmail.com>

---
+++ review/src/main/java/com/haot/review/application/service/ReviewService.java
@@ -0,0 +1,40 @@
+package com.haot.review.application.service;
+
+import com.haot.review.application.dtos.req.ReviewCreateRequest;
+import com.haot.review.application.dtos.res.ReviewGetResponse;
+import com.haot.review.domain.model.Review;
+import com.haot.review.domain.model.ReviewImage;
+import com.haot.review.domain.repository.ReviewImageRepository;
+import com.haot.review.domain.repository.ReviewRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+  private final ReviewRepository reviewRepository;
+  private final ReviewImageRepository reviewImageRepository;
+  private final MockS3Service mockS3Service;
+
+  @Transactional
+  public ReviewGetResponse createReview(String userId, ReviewCreateRequest request) {
+
+    // mock S3 서비스로 이미지를 업로드 및 String List 반환
+    List<String> imageUrls = mockS3Service.uploadImages(request.images());
+
+    Review review = Review.createReview(userId, request.contents(), request.lodgeId());
+    reviewRepository.save(review);
+
+    List<ReviewImage> reviewImages = imageUrls.stream()
+        .map(url -> ReviewImage.create(review, url))
+        .toList();
+
+    review.getImages().addAll(reviewImages);
+    reviewImageRepository.saveAll(reviewImages);
+
+    return ReviewGetResponse.of(review);
+  }
+}
---
+++ review/src/main/java/com/haot/review/submodule/auditor/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.haot.review.submodule.auditor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+public class JpaConfig {
+
+}
