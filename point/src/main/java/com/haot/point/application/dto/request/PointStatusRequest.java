2e0902419608343882c1937568ba97fc2e8db066
jelliclesu <jelliclesu@naver.com>
2025-01-10 00:07:28 +0900

    [Fix] Point: 상태 변경 Request 수정 #100

--- point/src/main/java/com/haot/point/application/dto/request/PointStatusRequest.java
+++ point/src/main/java/com/haot/point/application/dto/request/PointStatusRequest.java
@@ -3,6 +3,8 @@ package com.haot.point.application.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record PointStatusRequest(
-        @NotBlank String contextId
+        @NotBlank String contextId,
+        @NotBlank String status,
+        String cancelType
 ) {
 }
