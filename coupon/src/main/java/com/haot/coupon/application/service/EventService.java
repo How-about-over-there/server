7ba6801ac24b9aaa40a9687e23a284998beb899f
kimsung3113 <132237905+kimsung3113@users.noreply.github.com>
2025-01-11 17:43:29 +0900

    이벤트 조회 API 구현 (#127)
    [feat]이벤트 조회 API 구현

--- coupon/src/main/java/com/haot/coupon/application/service/EventService.java
+++ coupon/src/main/java/com/haot/coupon/application/service/EventService.java
@@ -1,7 +1,13 @@
 package com.haot.coupon.application.service;
 
+import com.haot.coupon.application.dto.request.events.EventSearchRequest;
 import com.haot.coupon.application.dto.response.events.EventSearchResponse;
+import com.haot.submodule.role.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface EventService {
     EventSearchResponse getEvent(String eventId);
+
+    Page<EventSearchResponse> searchEvent(Role userRole, EventSearchRequest request, Pageable pageable);
 }
--- coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponEventRepository.java
+++ coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponEventRepository.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface CouponEventRepository extends JpaRepository<CouponEvent, String> {
+public interface CouponEventRepository extends JpaRepository<CouponEvent, String>, CouponEventCustomRepository {
 
     boolean existsByCouponIdAndIsDeleteFalse(String CouponId);
 
