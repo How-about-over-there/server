6f94922a3c0175d56d771f0f5948934a2e9e83b0
wooseok <wooseok6008@gmail.com>
2025-01-11 19:39:03 +0900

    [Feat] Reservation: CustomReservationException #133
    - ErrorCode 추가

--- reservation/src/main/java/com/haot/reservation/common/response/enums/ErrorCode.java
+++ reservation/src/main/java/com/haot/reservation/common/response/enums/ErrorCode.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode implements ResCodeIfs {
 
 
+  RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "7100", "예약 내역을 찾을 수 없습니다"),
+  UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, "7101", "본인의 예약 내역만 조회할 수 있습니다."),
   INVALID_LODGE_RESPONSE_EXCEPTION(HttpStatus.BAD_REQUEST, "7001", "lodge response is invalid"),
   DATE_UNAVAILABLE_EXCEPTION(HttpStatus.BAD_REQUEST, "7002", "예약이 불가능한 날짜입니다.");
 
