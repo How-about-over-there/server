7096601c46634f0fbc28d4e2ea1582ac7ce3a1f0
kimsung3113 <kimsung1017@naver.com>
2025-01-12 23:45:41 +0900

    [fix] coupons : N+1 문제 해결 #134

--- coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponEventRepository.java
+++ coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponEventRepository.java
@@ -3,6 +3,8 @@ package com.haot.coupon.infrastructure.repository;
 import com.haot.coupon.domain.model.CouponEvent;
 import com.haot.coupon.domain.model.enums.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,7 +16,10 @@ public interface CouponEventRepository extends JpaRepository<CouponEvent, String
 
     List<CouponEvent> findByCouponIdAndEventEndDateIsAfterAndIsDeleteFalse(String id, LocalDateTime now);
 
-    Optional<CouponEvent> findByIdAndEventStatusAndIsDeleteFalse(String id, EventStatus eventStatus);
+    @Query("SELECT ce FROM CouponEvent ce JOIN FETCH ce.coupon c " +
+            "WHERE ce.id = :eventId AND ce.eventStatus = :eventStatus AND ce.isDelete = false")
+    Optional<CouponEvent> findByIdAndEventStatusAndIsDeleteFalse(@Param("eventId") String eventId,
+                                                                 @Param("eventStatus") EventStatus eventStatus);
 
     Optional<CouponEvent> findByIdAndIsDeleteFalse(String eventId);
 }
