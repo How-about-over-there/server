82f012ffddef2357fb367d3c2d4fd54ca029b0ab
jelliclesu <jelliclesu@naver.com>
2025-01-13 17:38:15 +0900

    [Fix] Payment: 예약 상태 변경 Feign 호출 추가 #135

--- payment/src/main/java/com/haot/payment/PaymentApplication.java
+++ payment/src/main/java/com/haot/payment/PaymentApplication.java
@@ -2,11 +2,13 @@ package com.haot.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {
 		"com.haot.payment",
 		"com.haot.submodule"
 })
+@EnableFeignClients
 public class PaymentApplication {
 
 	public static void main(String[] args) {
