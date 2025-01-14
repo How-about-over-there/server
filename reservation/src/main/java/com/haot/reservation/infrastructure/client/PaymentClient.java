32e8cdbc978154d440d80cc35390bd8fe7395b88
wooseok <wooseok6008@gmail.com>
2025-01-14 12:08:24 +0900

    [Feat] Reservation: PaymentClient 메서드 추가 #149

--- reservation/src/main/java/com/haot/reservation/infrastructure/client/PaymentClient.java
+++ reservation/src/main/java/com/haot/reservation/infrastructure/client/PaymentClient.java
@@ -1,11 +1,14 @@
 package com.haot.reservation.infrastructure.client;
 
 import com.haot.reservation.common.response.ApiResponse;
+import com.haot.reservation.infrastructure.dtos.payment.PaymentCancelRequest;
 import com.haot.reservation.infrastructure.dtos.payment.PaymentCreateRequest;
+import com.haot.reservation.infrastructure.dtos.payment.PaymentResponse;
 import com.haot.submodule.role.Role;
 import jakarta.validation.Valid;
 import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,7 +17,15 @@ import org.springframework.web.bind.annotation.RequestHeader;
 public interface PaymentClient {
 
   @PostMapping("/api/v1/payments")
-  ApiResponse<Map<String, Object>> createPayment(@Valid @RequestBody PaymentCreateRequest request,
+  ApiResponse<Map<String, Object>> createPayment(
+      @Valid @RequestBody PaymentCreateRequest request,
       @RequestHeader("X-User-Id") String userId,
-      @RequestHeader("X-User-Role") Role role);
+      @RequestHeader("X-User-Role") Role role
+  );
+
+  @PostMapping("/api/v1/payments/{paymentId}/cancel")
+  ApiResponse<PaymentResponse> cancelPayment(
+      @Valid @RequestBody PaymentCancelRequest request,
+      @PathVariable String paymentId
+  );
 }
