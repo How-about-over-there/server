70b0b177bb25f6941774902078b36e9940ca13e4
jelliclesu <jelliclesu@naver.com>
2025-01-15 18:49:16 +0900

    [Test] Payment: 결제 생성 컨틀롤러 테스트 수정 #165

--- payment/src/test/java/com/haot/payment/presentation/controller/PaymentControllerTest.java
+++ payment/src/test/java/com/haot/payment/presentation/controller/PaymentControllerTest.java
@@ -3,10 +3,14 @@ package com.haot.payment.presentation.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.haot.payment.application.dto.request.PaymentCancelRequest;
 import com.haot.payment.application.dto.request.PaymentCreateRequest;
+import com.haot.payment.application.dto.response.PaymentResponse;
+import com.haot.payment.application.service.PaymentService;
+import com.haot.submodule.role.Role;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -14,6 +18,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,20 +33,30 @@ class PaymentControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private PaymentService paymentService;
+
     @Test
-    @DisplayName("결제 생성 테스트")
+    @DisplayName("결제 생성 성공 테스트")
     void createPayment() throws Exception {
         // Given: 테스트 데이터
         PaymentCreateRequest request = new PaymentCreateRequest("USER-UUID", "RESERVATION-UUID", 100000.0, "CARD");
+        PaymentResponse response = new PaymentResponse("PAYMENT-UUID", "USER-UUID", "RESERVATION-UUID", null, 100000.0,null, "CARD", "READY");
+
+        // Mocking: PaymentService 동작 설정
+        when(paymentService.createPayment(request, "USER-UUID", Role.USER)).thenReturn(response);
 
         // When: API 호출 및 결과 받기
         MvcResult result = mockMvc.perform(post("/api/v1/payments")
+                        .header("X-User-Id", "USER-UUID")
+                        .header("X-User-Role", "USER")
+                        .header("Authorization", "BearerToken")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.statusCode").value("8000"))
-                .andExpect(jsonPath("$.status").value("Success"))
-                .andExpect(jsonPath("$.data.paymentId").exists())
+                .andExpect(jsonPath("$.status").value("SUCCESS"))
+                .andExpect(jsonPath("$.data.payment.paymentId").exists())
                 .andReturn(); // 호출 결과를 MvcResult 로 반환
 
         // Then: 요청 결과 확인
@@ -105,7 +120,7 @@ class PaymentControllerTest {
     @DisplayName("결제 취소 테스트")
     void cancelPayment() throws Exception {
         // Given: 테스트 데이터
-        PaymentCancelRequest request = new PaymentCancelRequest(100000.0, "CARD");
+        PaymentCancelRequest request = new PaymentCancelRequest("단순 변심");
         String paymentId = "PAYMENT-UUID1";
         // When: API 호출 및 결과 받기
         MvcResult result = mockMvc.perform(get("/api/v1/payments/{paymentId}", paymentId)
