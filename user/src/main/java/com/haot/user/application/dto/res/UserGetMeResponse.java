79f0253dc5b26b23c3ad247db6f58adb27272b54
chanwookK <kan0202@naver.com>
2025-01-15 11:57:18 +0900

    [Feat] User: 나의 정보 조회 Response 변경

--- user/src/main/java/com/haot/user/application/dto/res/UserGetMeResponse.java
+++ user/src/main/java/com/haot/user/application/dto/res/UserGetMeResponse.java
@@ -1,7 +1,9 @@
 package com.haot.user.application.dto.res;
 
+import com.haot.user.domain.model.User;
 import com.haot.user.domain.model.enums.Gender;
 import com.haot.submodule.role.Role;
+import java.time.LocalDate;
 import lombok.Builder;
 
 @Builder
@@ -10,7 +12,7 @@ public record UserGetMeResponse(
     String password,
     String email,
     String phoneNumber,
-    String birthDate,
+    LocalDate birthDate,
     Gender gender,
     String preferredLanguage,
     String currency,
@@ -19,5 +21,21 @@ public record UserGetMeResponse(
     Role role
 
 ) {
+
+  public static UserGetMeResponse of(User user) {
+    return UserGetMeResponse.builder()
+        .name(user.getName())
+        .password(user.getPassword())
+        .email(user.getEmail())
+        .phoneNumber(user.getPhoneNumber())
+        .birthDate(user.getBirthDate())
+        .gender(user.getGender())
+        .preferredLanguage(user.getPreferredLanguage())
+        .currency(user.getCurrency())
+        .profileImageUrl(user.getProfileImageUrl())
+        .address(user.getAddress())
+        .role(user.getRole())
+        .build();
+  }
 
 }
