c4f343751fc6913c17bbbad06676a5d7346f66d2
chanwookK <kan0202@naver.com>
2025-01-15 12:00:59 +0900

    [Feat] User: 유저 ID를 통한 조회 API ResponseDTO 수정

--- user/src/main/java/com/haot/user/application/dto/res/AdminUserGetResponse.java
+++ user/src/main/java/com/haot/user/application/dto/res/AdminUserGetResponse.java
@@ -1,27 +1,57 @@
 package com.haot.user.application.dto.res;
 
 import com.haot.submodule.role.Role;
+import com.haot.user.domain.model.User;
+import com.haot.user.domain.model.enums.Gender;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record AdminUserGetResponse(
     String userId,
     String name,
     String password,
     String email,
     String phoneNumber,
-    String birthDate,
-    String gender,
+    LocalDate birthDate,
+    Gender gender,
     String preferredLanguage,
     String currency,
     String profileImageUrl,
     String address,
     Role role,
     Boolean isDeleted,
-    String createdAt,
+    LocalDateTime createdAt,
     String createdBy,
-    String updatedAt,
+    LocalDateTime updatedAt,
     String updatedBy,
-    String deletedAt,
+    LocalDateTime deletedAt,
     String deletedBy
 ) {
+
+  public static AdminUserGetResponse of(User user) {
+    return AdminUserGetResponse.builder()
+        .userId(user.getId())
+        .name(user.getName())
+        .password(user.getPassword())
+        .email(user.getEmail())
+        .phoneNumber(user.getPhoneNumber())
+        .birthDate(user.getBirthDate())
+        .gender(user.getGender())
+        .preferredLanguage(user.getPreferredLanguage())
+        .currency(user.getCurrency())
+        .profileImageUrl(user.getProfileImageUrl())
+        .address(user.getAddress())
+        .role(user.getRole())
+        .isDeleted(user.isDeleted())
+        .createdAt(user.getCreatedAt())
+        .createdBy(user.getCreateBy())
+        .updatedAt(user.getUpdatedAt())
+        .updatedBy(user.getUpdatedBy())
+        .deletedAt(user.getDeleteAt())
+        .deletedBy(user.getDeletedBy())
+        .build();
+  }
 
 }
