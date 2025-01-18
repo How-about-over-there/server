f4e23d8eab680a64d3da48c23a2ed59b70983564
jelliclesu <jelliclesu@naver.com>
2025-01-18 15:58:11 +0900

    [Feat] Point: CacheConfig 및 Serializer 추가 #170

--- point/src/main/java/com/haot/point/application/dto/response/PageResponse.java
+++ point/src/main/java/com/haot/point/application/dto/response/PageResponse.java
@@ -2,14 +2,17 @@ package com.haot.point.application.dto.response;
 
 import org.springframework.data.domain.Page;
 
+import java.io.Serializable;
 import java.util.List;
 
-public record PageResponse<T> (
+public record PageResponse<T extends Serializable>(
         int totalPages,
         int pageNumber,
         List<T> content
-) {
-    public static <T> PageResponse<T> of(Page<T> page){
+) implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public static <T extends Serializable> PageResponse<T> of(Page<T> page) {
         return new PageResponse<>(
                 page.getTotalPages(),
                 page.getNumber() + 1,
