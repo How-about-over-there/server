74d7f74891a9d84f23c6ec1c9100c67e47a2cba5
kimsung3113 <kimsung1017@naver.com>
2025-01-22 11:02:30 +0900

    [remove] coupons : 사용하지 않는 JPQL 삭제 #143

--- coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponRepository.java
+++ coupon/src/main/java/com/haot/coupon/infrastructure/repository/CouponRepository.java
@@ -18,28 +18,8 @@ public interface CouponRepository extends JpaRepository<Coupon, String> {
     Optional<Coupon> findByIdAndIsDeleteFalse(String couponId);
 
     @Modifying
-    @Query(value = "update coupon.p_coupon SET issued_quantity = issued_quantity + 1 WHERE id = :id", nativeQuery = true)
-    int increaseIssuedQuantity(@Param("id") String id);
-
-    @Modifying
     @Query(value = "update coupon.p_coupon SET issued_quantity = issued_quantity + :issuedCount WHERE id = :id", nativeQuery = true)
     void increaseBatchIssuedQuantity(@Param("id") String id, @Param("issuedCount") Integer issuedCount);
-
-    // userid와 couponid가 맞는 userCoupon테이블에 있으면 null return, 없으면 coupon return
-    @Query(value = """
-        SELECT c.* 
-        FROM coupon.p_coupon c 
-        WHERE c.id = :couponId 
-          AND c.is_delete = false 
-          AND NOT EXISTS (
-              SELECT 1 
-              FROM coupon.p_user_coupon uc 
-              WHERE uc.coupon_id = c.id 
-                AND uc.user_id = :userId 
-                AND uc.is_delete = false
-        )
-    """, nativeQuery = true)
-    Optional<Coupon> findCouponIfNoUserCoupon(@Param("couponId") String couponId, @Param("userId") String userId);
 
     @Query(value = """
         SELECT uc.user_id 
