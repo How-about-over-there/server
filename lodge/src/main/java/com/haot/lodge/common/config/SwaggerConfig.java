2b3f6ad4a8ffefb850dff8419ea087e3fc92dc4a
kimsung3113 <132237905+kimsung3113@users.noreply.github.com>
2025-01-25 15:12:21 +0900

    Gateway Swagger 통합 설정
    Gateway Swagger 통합 설정

--- lodge/src/main/java/com/haot/lodge/common/config/SwaggerConfig.java
+++ lodge/src/main/java/com/haot/lodge/common/config/SwaggerConfig.java
@@ -16,7 +16,8 @@ import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         servers = {
-                @Server(url = "http://localhost:19091", description = "Local")
+                @Server(url = "http://gateway-service:19091", description = "Gateway URL"),
+                @Server(url = "http://localhost:19091", description = "Local Gateway URL")
         }
 )
 @Configuration
@@ -24,34 +25,51 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
-                .components(apiComponents())
-                .security(apiSecurity())
-                .info(apiInfo())
+                .info(new Info()
+                        .title("Haot-Lodge-Service")
+                        .description("HAOT 숙소 서비스")
+                        .version("v1"))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .addServersItem(new io.swagger.v3.oas.models.servers.Server().url("/"));
     }
 
-    private Info apiInfo() {
-        return new Info()
-                .title("Haot-Lodge-Service")
-                .description("HAOT 숙소 서비스")
-                .version("v1");
-    }
-
-    private Components apiComponents() {
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
-        return new Components().addSecuritySchemes("bearerAuth", securityScheme);
-    }
-
-    private List<SecurityRequirement> apiSecurity() {
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-        return Arrays.asList(securityRequirement);
-    }
+//    @Bean
+//    public OpenAPI openAPI() {
+//
+//        return new OpenAPI()
+//                .components(apiComponents())
+//                .security(apiSecurity())
+//                .info(apiInfo())
+//                .addServersItem(new io.swagger.v3.oas.models.servers.Server().url("/"));
+//    }
+//
+//    private Info apiInfo() {
+//        return new Info()
+//                .title("Haot-Lodge-Service")
+//                .description("HAOT 숙소 서비스")
+//                .version("v1");
+//    }
+//
+//    private Components apiComponents() {
+//        SecurityScheme securityScheme = new SecurityScheme()
+//                .type(SecurityScheme.Type.HTTP)
+//                .scheme("bearer")
+//                .bearerFormat("JWT")
+//                .in(SecurityScheme.In.HEADER)
+//                .name("Authorization");
+//        return new Components().addSecuritySchemes("bearerAuth", securityScheme);
+//    }
+//
+//    private List<SecurityRequirement> apiSecurity() {
+//        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+//        return Arrays.asList(securityRequirement);
+//    }
 
 }
